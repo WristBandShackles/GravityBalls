f82305e353d7908a51fe36261609e13a1e40d47c
WristBandShackles <103238301+WristBandShackles@users.noreply.github.com>
2022-04-14 20:45:21 -0700

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,6 +1,9 @@
+// Header
 var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 
+
+// Variables
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -16,11 +19,14 @@ var colors = [
 	'#e5383b'
 ];
 
-var gravity = 0.2;
-var friction = 0.98;
+// Storage Particle Mass
+var particleArray = [];
 
-var dxx = 0.0;
-var dyy = 0.2;
+var friction = 0.98; // 0 - 1 only
+
+// Gravity
+var accelerationX = 0.0;
+var accelerationY = 0.02;
 
 
 
@@ -53,89 +59,99 @@ function randomColor(colors) {
 	return colors[Math.floor(Math.random() * colors.length)];
 }
 
+function distance(x1, y1, x2, y2) {
+	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+}
+
+function isThereContact(particle1, particle2) {
+	if (!(particle1 === particle2)) {
+		return 	distance(particle1.x, particle1.y, particle2.x, particle2.y) <= particle1.radius + particle2.radius;
+	} else {
+		return false;
+	}
+	
+}
+
+function elasticCollision(particle1, particle2) {
+
+	if (distance(particle1.x, particle1.y, particle2.x, particle2.y) >= particle1.radius + particle2.radius - 20) {
+		// Mass
+		const m1 = particle1.mass;
+		const m2 = particle2.mass;
+
+		// Initial Velocity
+		//// X
+		const u1x = particle1.velocity.x;
+		const u2x = particle2.velocity.x;
+		//// Y
+		const u1y = particle1.velocity.y;
+		const u2y = particle2.velocity.y;
+		
+		// Final Velocity
+		//// X
+		const v1x = u1x * (m1 - m2) / (m1 + m2) + (u2x * 2 * m2) / (m1 + m2);
+		const v2x = u2x * (m2 - m1) / (m1 + m2) + (u1x * 2 * m1) / (m1 + m2);
+		//// Y
+		const v1y = u1y * (m1 - m2) / (m1 + m2) + (u2y * 2 * m2) / (m1 + m2);
+		const v2y = u2y * (m2 - m1) / (m1 + m2) + (u1y * 2 * m1) / (m1 + m2);
+
+		// Load Final Velocity
+		particle1.velocity.x = v1x;
+		particle1.velocity.y = v1y;
+
+		particle2.velocity.x = v2x;
+		particle2.velocity.y = v2y;
+	}
+
+}
+
 
 
 
 // Objects
-function Ball(x, y, dx, dy, dxx, dyy, radius, color, ball) {
+function Particle(x, y, velocityX, velocityY, accelerationX, accelerationY, radius, color) {
+
+	// Position
 	this.x = x;
 	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
-	this.dxx = dxx;
-	this.dyy = dyy;
-	this.radius = radius;
-	this.mass = radius;
-	this.color = color;
-	this.ball = ball;
 
-	this.getX = function(){
-		return this.x;
-	};
-
-	this.getY = function() {
-		return this.y;
-	};
-
-	this.flipDX = function() {
-		this.dx = -this.dx;
-	};
-
-	this.flipDY = function() {
-		this.dy = -this.dy;
-	};
-
-	this.getRadius = function() {
-		return this.radius;
-	};
-
-	this.isThereContact = function(ball) {
-		if ( !(this === ball) && Math.sqrt(((ball.getX() - this.x) * (ball.getX() - this.x)) + ((ball.getY() - this.y) * (ball.getY() - this.y))) < ball.getRadius() + this.radius){
-			return true;
-		} else {
-			return false;
-		}
-	};
-
-	this.elasticCollision = function(ball) {
-		//X collisions
-		this.dx = (((this.mass - ball.mass) / (this.mass + ball.mass)) * this.dx) + (((2 * ball.mass) / (this.mass + ball.mass)) * ball.dx);
-		ball.dx = (((ball.mass - this.mass) / (this.mass + ball.mass)) * ball.dx) + (((2 * this.mass) / (this.mass + ball.mass)) * this.dx);
-
-		//Y collisions
-		this.dy = (((this.mass - ball.mass) / (this.mass + ball.mass)) * this.dy) + (((2 * ball.mass) / (this.mass + ball.mass)) * ball.dy);
-		ball.dy = (((ball.mass - this.mass) / (this.mass + ball.mass)) * ball.dy) + (((2 * this.mass) / (this.mass + ball.mass)) * this.dy);
+	// Velocity
+	this.velocity = {
+		x: velocityX,
+		y: velocityY
 	}
 
+	// Acceleration
+	this.acceleration = {
+		x: accelerationX,
+		y: accelerationY
+	}
 
+	// Description
+	this.radius = radius;
+	this.mass = radius/3;
+	this.color = color;
+
+	// Each update call represents one tick of time and moves the particle forward
 	this.update = function() {
-
-		for(let i = 0; i < ballArray.length; i++) {
-			if (ballArray[i].isThereContact(this)) {
-				ballArray[i].elasticCollision(this);
-			}
-		}
-
-		if (this.y + this.radius + this.dy > canvas.height || this.y - this.radius <= 0) {
-			this.dy = -this.dy * friction;
+		if (this.y + this.radius + this.velocity.y > canvas.height || this.y - this.radius <= 0) {
+			this.velocity.y = -this.velocity.y * friction;
 			this.dx = this.dx * friction;
 		} else {
-			this.dy += this.dyy;
+			this.velocity.y += this.acceleration.y;
 		}
 
 		if (this.x + this.radius >= canvas.width || this.x - this.radius <= 0) {
-			this.dx = -this.dx * friction;
-			this.dy = this.dy * friction;
+			this.velocity.x = -this.velocity.x * friction;
+			this.velocity.y = this.velocity.y * friction;
 		} else {
-			this.dx += this.dxx;
+			this.velocity.x += this.acceleration.x;
 		}
 
-		this.y += this.dy; // + (1/2 * this.dyy) Potential addition for better phys;
-		this.x += this.dx;
-		this.draw();
+		this.y += this.velocity.y; //TODO////// + (1/2 * this.dyy) Potential addition for better phys;
+		this.x += this.velocity.x;
 	};
 
-	
 	this.draw = function() {
 		c.beginPath();
 		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);	
@@ -148,40 +164,47 @@ function Ball(x, y, dx, dy, dxx, dyy, radius, color, ball) {
 
 
 // Implementation
-var ballArray = [];
-
 function init() {
-	ballArray = [];
+	particleArray = [];
 
 	for (let i = 0; i < 200; i++) {
-		var radius = randomIntFromRange(8, 20);
-		var x = randomIntFromRange(radius, canvas.width - radius);
-		var y = randomIntFromRange(1 + radius, canvas.height - radius);
-		var dx = randomIntFromRange(-3, 3)
-		var dy = randomIntFromRange(-2, 2)
+		let radius = randomIntFromRange(10, 25);
+		let x = randomIntFromRange(0 + radius, innerWidth - radius);
+		let y = randomIntFromRange(0 + radius, innerHeight - radius);
+		let velocityX = randomIntFromRange(-2, 2);
+		let velocityY = randomIntFromRange(-2, 2);
 
-		var ball = new Ball(x, y, dx, dy, dxx, dyy, radius, randomColor(colors));
+		let particle = new Particle(x, y, velocityX, velocityY, accelerationX, accelerationY, radius, randomColor(colors));
+		particleArray.push(particle);
 
-		for (let k = 0; k < ballArray.length; k++) {
-			if (ballArray[k].isThereContact(ball)) {
-				ball.x = randomIntFromRange(radius, canvas.width - radius);
-				ball.y = randomIntFromRange(1 + radius, canvas.height - radius);
-				k = 0;
+		for(let j = 0; j < particleArray.length; j++) {
+			if(isThereContact(particle, particleArray[j])) {
+				particle.x = randomIntFromRange(0 + particle.radius, innerWidth - particle.radius);
+				particle.y = randomIntFromRange(0 + particle.radius, innerHeight - particle.radius);
+				j = 0;
 			}
 		}
 
-	    ballArray.push(ball);
+
 	}
 }
 
 // Animation Loop
 function animate() {
 	requestAnimationFrame(animate);
-
 	c.clearRect(0, 0, canvas.width, canvas.height);
 
-	for (let i = 0; i < ballArray.length; i++) {
-		ballArray[i].update();
+	for (let i = 0; i < particleArray.length; i++) {
+
+		particleArray[i].draw();
+
+		for (let j = 0; j < particleArray.length; j++) {
+			if(isThereContact(particleArray[i], particleArray[j])) {
+				elasticCollision(particleArray[i], particleArray[j]);
+			}
+		}
+
+		particleArray[i].update();
 	}
 }
 
